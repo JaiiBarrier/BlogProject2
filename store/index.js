e24605e25459feb0a4a75af17ef1a8d4362527c9
jaiijaii <109163810+JaiiBarrier@users.noreply.github.com>
2022-08-26 00:00:05 -0500

    Fixed Cookies Error
    Fixed the error by adding "let" to the code

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-import Vuex from 'vuex';
+import Vuex from "vuex";
 import Cookie from "js-cookie";
 
 const createStore = () => {
@@ -9,23 +9,23 @@ const createStore = () => {
     },
     mutations: {
       setPosts(state, posts) {
-          state.loadedPosts = posts;
-        },
-        addPost(state, post) {
-          state.loadedPosts.push(post);
-        },
-        editPost(state, editedPost) {
-          const postIndex = state.loadedPosts.findIndex(
-            post => post.id === editedPost.id
-          );
-            state.loadedPosts[postIndex] = editedPost;
-        },
-        setToken(state, token ) {
-          state.token = token; 
-        },
-        clearToken(state) {
-          state.token = null;
-        }
+        state.loadedPosts = posts;
+      },
+      addPost(state, post) {
+        state.loadedPosts.push(post);
+      },
+      editPost(state, editedPost) {
+        const postIndex = state.loadedPosts.findIndex(
+          post => post.id === editedPost.id
+        );
+        state.loadedPosts[postIndex] = editedPost;
+      },
+      setToken(state, token) {
+        state.token = token;
+      },
+      clearToken(state) {
+        state.token = null;
+      }
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
@@ -36,33 +36,33 @@ const createStore = () => {
             for (const key in data) {
               postsArray.push({ ...data[key], id: key });
             }
-              vuexContext.commit('setPosts', postsArray);
-            })
-            .catch(e => context.error(e));
-        },
-        addPost(vuexContext, post) {
-          const createdPost = {
-            ...post, 
-            updatedDate: new Date()
-          };
-          return this.$axios
-            .$post(
-              "https://nuxject-default-rtdb.firebaseio.com/posts.json?auth=" + 
-                vuexContext.state.token, 
-              createdPost
-            )
-            .then(data => {
-              vuexContext.commit("addPost", {...createdPost, id: data.name})
+            vuexContext.commit("setPosts", postsArray);
+          })
+          .catch(e => context.error(e));
+      },
+      addPost(vuexContext, post) {
+        const createdPost = {
+          ...post,
+          updatedDate: new Date()
+        };
+        return this.$axios
+          .$post(
+            "https://nuxject-default-rtdb.firebaseio.com/posts.json?auth=" + 
+              vuexContext.state.token,
+            createdPost
+          )
+          .then(data => {
+            vuexContext.commit("addPost", { ...createdPost, id: data.name });
           })
           .catch(e => console.log(e));
-        },
-        editPost(vuexContext, editedPost) {
-          return this.$axios
+      },
+      editPost(vuexContext, editedPost) {
+        return this.$axios
           .$put(
             "https://nuxject-default-rtdb.firebaseio.com/posts/" + 
-              editedPost.id + 
-              ".json?auth=" + 
-              vuexContext.state.token, 
+              editedPost.id +
+              ".json?auth=" +
+              vuexContext.state.token,
             editedPost
           )
           .then(res => {
@@ -70,36 +70,36 @@ const createStore = () => {
           })
           .catch(e => console.log(e));
       },
-        setPosts(vuexContext, posts) {
-            vuexContext.commit("setPosts", posts);
+      setPosts(vuexContext, posts) {
+        vuexContext.commit("setPosts", posts);
       },
-        authenticateUser(vuexContext, authData) {
-          let authUrl = 
-            "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + 
-        process.env.fbAPIKey;
-      if (!authData.isLogin) {
-        authUrl =
-          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + 
-          process.env.fbAPIKey; 
-      }
-      return this.$axios
-        .$post(authUrl,  {
+      authenticateUser(vuexContext, authData) {
+        let authUrl =
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
+          process.env.fbAPIKey;
+        if (!authData.isLogin) {
+          authUrl =
+            "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
+            process.env.fbAPIKey;
+        }
+        return this.$axios
+          .$post(authUrl, {
             email: authData.email,
             password: authData.password,
             returnSecureToken: true
           })
-        .then(result => {
-          vuexContext.commit("setToken", result.idToken);
-          localStorage.setItem("token", result.idToken);
-          localStorage.setItem(
-            "tokenExpiration", 
-            new Date().getTime() + result.expiresIn * 1000
-          );
-          Cookie.set('jwt', result.idToken);
-          Cookie.set(
-            "expirationDate", 
-            new Date().getTime() + result.expiresIn * 1000
-          );
+          .then(result => {
+            vuexContext.commit("setToken", result.idToken);
+            localStorage.setItem("token", result.idToken);
+            localStorage.setItem(
+              "tokenExpiration",
+              new Date().getTime() + result.expiresIn * 1000
+            );
+            Cookie.set("jwt", result.idToken);
+            Cookie.set(
+              "expirationDate",
+              new Date().getTime() + result.expiresIn * 1000
+            );
           vuexContext.dispatch("setLogoutTimer" , result.expiresIn * 1000);
         })
         .catch(e => console.log(e));
@@ -110,46 +110,53 @@ const createStore = () => {
         }, duration);
       },
       initAuth(vuexContext, req) {
+        let token;
+        let expirationDate;
         if (req) {
           if (!req.headers.cookie) {
             return;
           }
           const jwtCookie = req.headers.cookie
-            .split(';')
+            .split(";")
             .find(c => c.trim().startsWith("jwt="));
           if (!jwtCookie) {
             return;
           }
-          const token = jwtCookie.split('=')[1];
-          const expirationDate = req.headers.cookie
-            .split(';')
+          token = jwtCookie.split("=")[1];
+          expirationDate = req.headers.cookie
+            .split(";")
             .find(c => c.trim().startsWith("expirationDate="))
-            .split('=')[1];
+            .split("=")[1];
         } else {
-          const token = localStorage.getItem("token");
-          const expirationDate = localStorage.getItem("tokenExpiration");
+          token = localStorage.getItem("token");
+          expirationDate = localStorage.getItem("tokenExpiration");
 
         if (new Date().getTime() > +expirationDate || !token) {
           return;
          }
         }
-        
+
         vuexContext.dispatch(
           "setLogoutTimer", 
           +expirationDate - new Date().getTime()
         );
+
+
         vuexContext.commit("setToken", token);
       }
+
+
+
     },
     getters: {
       loadedPosts(state) {
         return state.loadedPosts;
-        },
+      },
       isAuthenticated(state) {
-        return state.token != null
+        return state.token != null;
       }
     }
   });
 };
 
-export default createStore
+export default createStore;
